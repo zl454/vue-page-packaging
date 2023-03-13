341449366e6638aee074f2b303233dd1e0ed7a55
zl454 <531720074@qq.com>
2023-03-14 00:34:53 +0800

    添加top样式，组件按需引入

--- src/views/design/layout/Top.vue
+++ src/views/design/layout/Top.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="design-layout__top">Top</div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <div class="design-layout__top">
+    <div class="middle">
+      <div style="display: flex">
+        <span class="page-name__label">页面名称： </span
+        ><a-input
+          class="page-name__input"
+          v-model.trim="pageName"
+          placeholder="请输入页面名称"
+        ></a-input>
+      </div>
+    </div>
+    <div :span="4" class="right">
+      <a-button class="btn">返回列表</a-button>
+      <a-button class="btn" type="primary">保存</a-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-// import HelloWorld from './view/HelloWorld.vue'
+const pageName = ref("");
+const toList = () => {};
+const savePage = () => {};
 </script>
 
 <style lang="scss" scoped>
@@ -18,5 +34,37 @@
   background: rgba(255, 255, 255, 1);
   box-shadow: 2px 0px 8px 0px rgba(188, 195, 206, 1);
   z-index: 3;
+  display: flex;
+  justify-content: flex-end;
+  .middle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #3f4245;
+    font-size: 18px;
+    font-weight: 600;
+    .page-name__label {
+      font-size: 15px;
+      font-weight: bold;
+      line-height: 35px;
+      color: #29ba9c;
+      white-space: nowrap;
+    }
+    .page-name__input {
+      width: 300px;
+    }
+  }
+  .right {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    line-height: 50px;
+    align-items: center;
+    margin-left: 30px;
+    .btn {
+      width: 96px;
+      height: 100%;
+    }
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [vue()], resolve: {
+  server: {
+    open: true
+  },
+  plugins: [vue(), AutoImport({
+    imports: ['vue'],
+  }),
+  Components({
+    dirs: ['src/components/'],
+    // 配置需要将哪些后缀类型的文件进行自动按需引入
+    extensions: ['vue', 'md'],
+    // 解析的 UI 组件库，这里以 Element Plus 和 Ant Design Vue 为例
+    resolvers: [AntDesignVueResolver()],
+  }),], resolve: {
     alias: {
       '@': resolve(__dirname, './src')
     }
