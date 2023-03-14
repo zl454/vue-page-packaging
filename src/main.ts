c476bb5c961b7d8cd92c5875d47028421a065818
zl454 <531720074@qq.com>
2023-03-14 23:39:41 +0800

    调整插件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { createApp } from "vue";
-import "./style.css";
-import router from "@/router";
+import { createApp } from 'vue'
+import './style.css'
+import router from '@/router'
 import pinia from '@/store'
-import App from "@/layout/Index.vue";
-
-createApp(App).use(router).use(pinia).mount("#app");
+import App from '@/layout/Index.vue'
+createApp(App).use(router).use(pinia).mount('#app')
